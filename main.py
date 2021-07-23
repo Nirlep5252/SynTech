decb8688afb706eee5da24c7d2a4fbaa49c8d4c7
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2021-07-23 13:48:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -20,15 +20,5 @@ async def on_ready():
     await bot.change_presence(
         activity=discord.Activity(type=discord.ActivityType.watching, name=f"Add text here"))
 
-@bot.command(name="ping", usage="", description="Display the bot's ping.")
-@commands.cooldown(1, 2, commands.BucketType.member)
-async def ping(ctx):
-    before = time.monotonic()
-    embed = discord.Embed(title="Ping!", description=f'Ping')
-    message = await ctx.send(embed=embed)
-    ping = (time.monotonic() - before) * 1000
-    embed2 = discord.Embed(title="Pong!", description=f'Api Ping  !  `{round(bot.latency * 1000)}` ms\nBot Ping ! `{int(ping)}` ms')
-    await message.edit(embed=embed2)
-
 load_dotenv('.env')
 bot.run(os.getenv('DISCORD_BOT_SECRET'))
