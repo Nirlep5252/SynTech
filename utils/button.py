96b9eae11dbf8d26ad1815558793576f0c7d1cf3
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2021-07-29 23:08:27 -0400

    Add files via upload

--- utils/button.py
+++ utils/button.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
 
-from config import Website_link
+from config import Website_link, MAIN_COLOR, VERIFIED, TICKET_EMOJI, CLOSE_EMOJI
+from utils.database import db
+
 
 class Button(discord.ui.View):
     def __init__(self):
@@ -19,3 +21,58 @@ class Button(discord.ui.View):
     @discord.ui.button(label='Don\'t hit me', style=discord.ButtonStyle.green)
     async def button_button(self, button, interaction):
         await interaction.response.send_message("Whyyyyyyyyy", ephemeral=True)
+
+class Verify(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="Verify", style=discord.ButtonStyle.green, emoji=f"{VERIFIED}", custom_id="verify_view:green")
+    async def verify(self, button, interaction):
+        role = interaction.guild.get_role(870159097524273262)
+        member_role = interaction.guild.get_role(870161379141763092)
+        await interaction.user.add_roles(role)
+        await interaction.user.add_roles(member_role)
+        embed = discord.Embed(title="Verified", description=f"You have been Verified, You can now chat with the other developers!", color=MAIN_COLOR).set_footer(text=f"Welcome to the server", icon_url=interaction.user.avatar.url)
+        await interaction.user.send(embed=embed)
+
+class Close(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="Close", style=discord.ButtonStyle.red, emoji=f"{CLOSE_EMOJI}", custom_id="close_view:red")
+    async def close(self, button, interaction):
+        e = db.collection.find_one(
+            {"ticket": int(interaction.channel.topic)})
+
+        if e is None:
+              await interaction.response.send_message("User has no ticket", ephemeral=True)
+
+        else:
+            a = db.collection.find_one({"ticket": int(interaction.channel.topic)})
+            db.collection.delete_one(a)
+            embed = discord.Embed(title="Closed", description=f"We hope your problem was fixed, thank you", color=MAIN_COLOR).set_footer(text="If you think this was a mistake dm a staff")
+            await interaction.user.send(embed=embed)
+            await interaction.channel.delete()
+
+class Ticket(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="Open Ticket", style=discord.ButtonStyle.green, emoji=f"{TICKET_EMOJI}", custom_id="ticket_view:green")
+    async def ticket(self, button, interaction):
+        e = db.collection.find_one({"ticket": interaction.user.id})
+        if e is None:
+         overwrites = {
+            interaction.guild.me: discord.PermissionOverwrite(read_messages=True),
+            interaction.user: discord.PermissionOverwrite(read_messages=True),
+            interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False)
+        }
+         channel = await interaction.guild.create_text_channel(name=f'ticket-{interaction.user.id}', overwrites=overwrites, topic=interaction.user.id)
+         embed = discord.Embed(title="Thank you!", description=f"Hello thank you for coming to us!.\nIf you need support please ping a staff.\nIf you have something to ask go ahead.", color=MAIN_COLOR)
+         await channel.send(embed=embed, view=Close())
+         tickets = {"ticket": interaction.user.id}
+         db.collection.insert_one(tickets)
+
+        else:
+            await interaction.response.send_message("You have a ticket open", ephemeral=True)
+
