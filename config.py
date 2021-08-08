f2c2ab2705e6d1fa8189e1ec19d45a37b5addc0c
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2021-08-07 22:32:53 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -5,12 +5,18 @@ from discord.ext import commands
 
 Website_link = 'https://github.com/Synterra-Technologies'
 
+#Color config
+
 MAIN_COLOR = discord.Color.green()
 ERROR_COLOR = discord.Color.red()
 WARN_COLOR = discord.Color.orange()
 NSFW_COLOR = discord.Color.purple()
 FUN_COLOR = discord.Color.blue()
+
+#Bot config
+
 DEVELOPER = 'Blue.#1270'
+VERSION = "1.3"
 PREFIXES = ['!']
 VERIFIED = '<a:check:870155042659921941>'
 TICKET_EMOJI = '🎟️'
@@ -18,6 +24,9 @@ CLOSE_EMOJI = '🛑'
 LOG_CHANNEL = 869369735886823455
 TICKETS_CATEGORY = 871207377750347796
 STAFF_ROLE = 870410239806165023
+
+#Global chat config
+
 GLOBAL_CHAT_WEBHOOK = f"{os.getenv('WEBHOOK_1')}"
 GLOBAL_CHAT_WEBHOOK_2 = f"{os.getenv('WEBHOOK_2')}"
 GLOBAL_CHAT_CHANNEL = 872195315535601745
